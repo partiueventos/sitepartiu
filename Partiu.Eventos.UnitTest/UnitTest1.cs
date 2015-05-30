f77904d884dde42989b3ad2f9d88b6608ad98d8c
partiueventos <partiueventos.net@gmail.com>
2015-05-30 11:13:20 -0300

    Primeiro Commit

---
+++ Partiu.Eventos.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Partiu.Eventos.UnitTest
+{
+    [TestClass]
+    public class UnitTestPartiu 
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
