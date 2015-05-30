4c36202965d5841ab26673f9d440dc27aa5df4f6
partiueventos <partiueventos.net@gmail.com>
2015-05-30 17:26:03 -0300

    Commit 300515

---
+++ Partiu.Eventos.Dominio/Entidades/Evento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partiu.Eventos.Dominio.Entidades
+{
+    class Evento
+    {
+        public int idEvento { get; set; }
+        public String nome { get; set; }
+        public String descricao { get; set; }
+        public DateTime data { get; set; }
+        public int views { get; set; }
+        public string local { get; set; }
+        public string telefone { get; set; }
+    }
+}
